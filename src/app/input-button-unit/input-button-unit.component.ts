e48085d08eea679f82a656b84d3c667da7b31432
Ewelina <49327529+Ewelina1994@users.noreply.github.com>
2020-11-21 15:14:57 +0100

    add button

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,15 +2,41 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
+  template: `
+  <p>The title is: {{title}}</p>
+  <input [value]="title" (keyup.enter)="changeTitle($event.target.value)">
+  <button (click)="changeTitle('Button Clicked!')">Save</button>`,
+
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {
 
   title='Hello World';
-  constructor() { }
+  constructor() {
+    this.title='I love Angular'
+  }
 
   ngOnInit(): void {
+    this.title='Angular CLI Rules!';
+    this.show();
+    setTimeout(() => {
+      this.title = 'This is not the title you are looking for';
+    }, 3000);
+  }
+
+  changeTitle(event) {
+    console.log(event);
+    this.title = event.target.value; // the original functionality still works
+  }
+
+  show() {
+    let x=10;
+    x+=10;
+    console.log(x);
+  }
+
+  generateTitle(): string {
+    return this.title;
   }
 
 }
